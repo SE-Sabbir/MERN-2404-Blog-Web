0d236682f216769fa9ab72526e5d2ad3907fec4b
MD SABBIR <63668842+SE-Sabbir@users.noreply.github.com>
2026-02-24 13:02:10 +0600

    vercel config

--- backend/router/index.js
+++ backend/router/index.js
@@ -1,10 +1,6 @@
 const express = require('express')
 const router = express.Router()
 
-router.use('/' ,(req , res)=>{
-    res.redirect(process.env.CLIENT_URL)
-})
-
 router.use('/auth' , require('./auth'))
 router.use('/blog' , require('./blog') )
 
